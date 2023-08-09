fa9c8552266158fb6c0f532bc4e292fe4475d9d1
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-08-10 01:14:43 +0530

    feat: Mode and Other testnet added

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,55 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { base, mainnet, optimism, zora } from "wagmi/chains";
+import {
+  base,
+  baseGoerli,
+  mainnet,
+  optimism,
+  optimismGoerli,
+  zora,
+  zoraTestnet,
+} from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
+const modeSepolia = {
+  id: 919,
+  name: "Mode Testnet",
+  network: "Filecoin — HyperSpace testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "ETH",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: {
+      http: ["https://sepolia.mode.network"],
+    },
+    default: {
+      http: ["https://sepolia.mode.network"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Mode Sepolia Explorer",
+      url: "https://sepolia.explorer.mode.network/",
+    },
+  },
+  testnet: true,
+};
+
 const { chains, publicClient } = configureChains(
-  [mainnet, optimism, zora, base],
+  [
+    mainnet,
+    optimism,
+    optimismGoerli,
+    zora,
+    zoraTestnet,
+    base,
+    baseGoerli,
+    modeSepolia,
+  ],
   [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
