74383673f1efe648210f96c6e00db5e9dd260a63
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-08-10 00:49:15 +0530

    feat: CLI tool files initialised

---
+++ CLITool/index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+// Global Variables
+
+async function welcome() {}
+
+// Processes
+console.clear();
+await welcome();
