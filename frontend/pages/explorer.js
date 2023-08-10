2ff7b2853c0f2ec02ea045b5cfa6605dec61f199
Architsharma7 <architsharma7911@gmail.com>
2023-08-10 21:06:20 +0530

    fix: functions for explorer,verifier and deployer added

--- frontend/pages/explorer.js
+++ frontend/pages/explorer.js
@@ -1,8 +1,131 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import ReturnedFunction from "@/components/ReturnedFunction";
+import {
+  analyzeABI,
+  contractDataType,
+  functionType,
+} from "@/functionality/analyzeABI";
+import { Registery_ABI, Registery_address } from "@/constants/constants";
+import { useAccount, useContract, useProvider } from "wagmi";
+import { Contract, Wallet } from "ethers";
+import { storeContract } from "@/functionality/storeData";
+import { explorerLink } from "@/constants/constants";
+import { useToast } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import ReturnedSourceCode from "@/components/ReturnedSourceCode";
+
+const private_key = process.env.NEXT_PUBLIC_PRIVATE_KEY;
 
 const Explorer = () => {
+  const router = useRouter();
+
+  //   const [readFunctions, setReadFunctions] = useState<functionType[]>();
+  //   const [writeFunctions, setWriteFunctions] = useState<functionType[]>();
+  const [showType, setShowType] = useState("");
+  //   const [constructors, setConstructors] = useState<functionType[]>();
+  const [contractExists, setContractExists] = useState(false);
+  //   const [contractData, setContractData] = useState<contractDataType>();
+  const [contractAddress, setContractAddress] = useState("");
+  const [ipfsURI, setIpfsURI] = useState("");
+  const [isReadActive, setIsReadActive] = useState(false);
+  const [isWriteActive, setIsWriteActive] = useState(false);
+  const [isSourceCodeActive, setIsSourceCodeActive] = useState(false);
+
+  const { address } = useAccount();
+  const provider = useProvider();
+  const Registery_Contract = useContract({
+    address: Registery_address,
+    abi: Registery_ABI,
+    signerOrProvider: provider,
+  });
+  const toast = useToast();
+  console.log(showType, "showtype here");
+  useEffect(() => {
+    const queryAddress = router.query.address;
+    if (queryAddress) {
+      setContractAddress(queryAddress);
+    }
+  }, [router.query]);
+
+  async function searchContract() {
+    if (!contractAddress) return;
+    try {
+      const response = await Registery_Contract?.getContractRecord(
+        contractAddress
+      );
+      toast({
+        title: "Address fetched!!!",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+      // console.log(response);
+      if (!response) {
+        toast({
+          title: "Contract does not exist",
+          description: "This contract does not exist in our registry",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+        // console.log("Contract does not exist");
+        setContractExists(false);
+        return;
+        /// notify that Contract doesnot Exists
+      }
+      setIpfsURI(response);
+      setContractExists(true);
+      fetchContractData(response);
+    } catch (error) {
+      toast({
+        title: `${error.reason}`,
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+      console.log(error);
+    }
+  }
+
+  async function fetchContractData(ipfsURL) {
+    const contractData = await (await fetch(ipfsURL)).json();
+    // console.log(contractData);
+
+    if (!contractData) {
+      toast({
+        title: "Contract Data not found",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+      // console.log("Contract Data not found");
+      return;
+    }
+    /// has bytecode , abi , code
+    setContractData(contractData);
+    setShowType("source");
+    setIsSourceCodeActive(true);
+    setIsReadActive(false);
+    setIsWriteActive(false);
+    getData(contractData.abi);
+    //set default to the contract Tab and show all the data there
+  }
+
+  /// issue with the ABI type
+  async function getData(abi) {
+    const data = await analyzeABI(abi);
+    // console.log(data);
+    setReadFunctions(data?.read);
+    setWriteFunctions(data?.write);
+
+    console.log(data, "getData");
+  }
+  console.log(contractData);
+  // now handle for the contract that does not exists
+  // send the user to deploy page but with a contractAddress , so that it will not deploy the contract again and verify the contract
+
   return (
     <div className="w-screen h-screen bg-gradient-to-br from-gray-50 via-sky-50 to-slate-50">
       <div className="flex flex-col justify-center mx-auto items-center">
@@ -15,14 +138,25 @@ const Explorer = () => {
           </div>
           <div className="flex mt-6">
             <div className="w-[700px] border border-gray-300 rounded-xl py-2 px-2 flex">
-              <input className="px-5 py-2 text-2xl rounded-2xl border border-black w-11/12"></input>
+              <input
+                input={contractAddress}
+                setInput={setContractAddress}
+                search={searchContract}
+                className="px-5 py-2 text-2xl rounded-2xl border border-black w-11/12"
+              ></input>
               <button className="mx-3 border border-gray-400 px-3 py-1 rounded-xl text-2xl">
                 <AiOutlineSearch />
               </button>
             </div>
           </div>
           <div className="mt-10">
-            <Tabs colorScheme='blue' size="lg" align="center" isFitted variant='enclosed'>
+            <Tabs
+              colorScheme="blue"
+              size="lg"
+              align="center"
+              isFitted
+              variant="enclosed"
+            >
               <TabList>
                 <Tab>Read Contract</Tab>
                 <Tab>Write Contract</Tab>
--- frontend/pages/verifier.js
+++ frontend/pages/verifier.js
@@ -1,6 +1,156 @@
 import React from "react";
+import ConstructorArguments from "./ConstructorArgs";
+import { deploy } from "../functionality/deployContract";
+import { useAccount, useProvider, useTransaction } from "wagmi";
+import { storeContract } from "../functionality/storeData";
+import { Contract, Wallet } from "ethers";
+import { Registery_ABI, Registery_address } from "../constants/constants";
+import { explorerLink } from "../constants/constants";
+import { useToast } from "@chakra-ui/react";
+const private_key = process.env.NEXT_PUBLIC_PRIVATE_KEY;
 
 const Verifier = () => {
+  const { address } = useAccount();
+  const provider = useProvider();
+  const toast = useToast();
+
+  const [contractName, setContractName] = useState("");
+  const [sourceCode, setSourceCode] = useState("");
+  // const [output, setOutput] = useState<{ abi: any[]; bytecode: string }>();
+
+  const [contractAddress, setContractAddress] = useState("");
+  const [error, setError] = useState("");
+
+  const [compiled, setCompiled] = useState(false);
+  const [ipfsLink, setIpfsLink] = useState("");
+
+  /// add the ENV thing and enable
+  const manager_wallet = new Wallet(private_key, provider);
+  const registery_contract = new Contract(
+    Registery_address,
+    Registery_ABI,
+    manager_wallet
+  );
+
+  /// contract with imports have to be managed , not yet handled
+  async function handleCompile() {
+    if (!sourceCode) {
+      toast({
+        title: "No source code",
+        description:
+          "You need to provide source code to perform compilation!!!",
+        status: "warning",
+        duration: 2000,
+        isClosable: true,
+      });
+      // console.log("no Source code set");
+      return;
+    }
+
+    /// For proper handling we can change the API call format
+    const response = await fetch("./api/compile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sourceCode }),
+    });
+
+    console.log(response);
+    const formattedResponse = (await response.json()).output;
+    // console.log(formattedResponse, "formatted response");
+
+    if (response.status == 200) {
+      setOutput(formattedResponse);
+      toast({
+        title: "Compilation successfull",
+        description:
+          "Your code was compiled succesfully, You can deploy your contract now.",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+      // console.log("Successfully Compiled");
+      setError("Successfully Compiled");
+      /// analyze the ABI and show const
+      // handleABI(formattedResponse.abi);
+
+      setCompiled(true);
+    } else {
+      setError(formattedResponse);
+      toast({
+        title: "Compilation error",
+        description: `${formattedResponse}`,
+        status: "error",
+        duration: 2700,
+        isClosable: true,
+      });
+    }
+  }
+
+  async function verifyContract() {
+    /// store the contract info on the Web3.storage and add the data , CID to a Contract or Web2 Database
+    if (!output && !contractAddress) {
+      toast({
+        title: "Contract Not Compiled OR Deployed!!!",
+        description: `This contract is either not deployed or compiled, which is necessary for contract verification`,
+        status: "error",
+        duration: 2800,
+        isClosable: true,
+      });
+      // console.log("Compile & Deploy the Contract first");
+      setError("Compile & Deploy the Contract first");
+      return;
+    }
+
+    const contractData = {
+      name: contractName,
+      address: contractAddress,
+      deployer: address,
+      abi: output?.abi,
+      bytecode: output?.bytecode,
+      code: sourceCode,
+    };
+
+    toast({
+      title: "Uploading to IPFS...",
+      status: "loading",
+      duration: 2000,
+      isClosable: true,
+    });
+    const CID = await storeContract(contractData);
+    const IPFSURL = `https://w3s.link/ipfs/${CID}`;
+    console.log(IPFSURL, "IPFSURL");
+    setIpfsLink(IPFSURL);
+    toast({
+      title: "IPFS URL",
+      description: `${IPFSURL}`,
+      status: "success",
+      duration: 2800,
+      isClosable: true,
+    });
+    /// Store the IPFS link somewhere
+
+    const tx = await registery_contract.addContractRecord(
+      contractAddress,
+      IPFSURL
+    );
+    toast({
+      title: "Adding Contract to Registry",
+      status: "loading",
+      duration: 2500,
+      isClosable: true,
+    });
+    await tx.wait();
+    // console.log("Record Added in the registery");
+    toast({
+      title: "Record Added in the Registry",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  }
+
   return (
     <div>
       <div className="w-screen h-full bg-gradient-to-br from-gray-50 via-sky-50 to-slate-50">
@@ -13,7 +163,12 @@ const Verifier = () => {
           </div>
           <div className="flex mt-6">
             <div>
-              <input className="px-5 py-2 text-2xl rounded-2xl border border-black w-[700px]"></input>
+              <input
+                onChange={(e) => {
+                  setContractAddress(e.target.value);
+                }}
+                className="px-5 py-2 text-2xl rounded-2xl border border-black w-[700px]"
+              ></input>
             </div>
           </div>
           <div className="mt-20 flex flex-col">
@@ -21,12 +176,26 @@ const Verifier = () => {
               <p className="text-xl font-mono">Paste Contract Code Here</p>
             </div>
             <div className="flex mt-6 justify-center mx-auto">
-              <textarea className="border border-black w-[700px] rounded-xl px-4 py-2 font-sans h-[500px]"></textarea>
+              <textarea
+                onChange={(e) => setSourceCode(e.target.value)}
+                className="border border-black w-[700px] rounded-xl px-4 py-2 font-sans h-[500px]"
+              ></textarea>
             </div>
             <div className="mt-7 mb-20 flex justify-center mx-auto">
-              <button className="px-10 py-1.5 rounded-xl border bg-gradient-to-r from-indigo-400 to-green-400 text-white text-xl hover:scale-110 duration-200">
+              <button
+                onClick={() => handleCompile()}
+                className="px-10 py-1.5 rounded-xl border bg-gradient-to-r from-indigo-400 to-green-400 text-white text-xl hover:scale-110 duration-200"
+              >
                 Compile
               </button>
+              {compiled && (
+                <button
+                  onClick={() => verifyContract()}
+                  className="px-10 py-1.5 rounded-xl border bg-gradient-to-r from-indigo-400 to-green-400 text-white text-xl hover:scale-110 duration-200"
+                >
+                  Verify
+                </button>
+              )}
             </div>
           </div>
         </div>
