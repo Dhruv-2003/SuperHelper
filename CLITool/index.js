b91b04be7c1b4dff33e673391cb812e0449077e7
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-08-11 18:32:19 +0530

    feat: CLI Compile method

--- CLITool/index.js
+++ CLITool/index.js
@@ -2,6 +2,8 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+import fsPromises from "fs/promises";
+import path from "path";
 
 // Global Variables
 
@@ -20,23 +22,73 @@ async function welcome() {
 async function askTask() {
   const answers = await inquirer.prompt({
     name: "Task",
-    type: "input",
+    type: "list",
     message: "What do you want to do ?",
-    default() {
-      return "compile";
-    },
+    choices: ["Compile", "Deploy", "Verify", "Search"],
   });
 
-  // convert answers in lowerCase
   // Check the Task type and process it
+  const choice = answers.Task;
+  // console.log(choice, answers);
+
+  if (choice == "Compile") {
+    compile();
+  } else if (choice == "Deploy") {
+  } else if (choice == "Verify") {
+  } else if (choice == "Search") {
+  } else {
+    console.log("WRONG CHOICE");
+    askTask();
+  }
 }
 
 async function compile() {
   /// Take the file Path
-  /// Get the file and then read it's content
-  /// Call the Compile API with the SourceCode
-  /// Parse the Output and display that
-  /// Show the Errors if present based on the API Res Code
+  const answers = await inquirer.prompt({
+    name: "Filepath",
+    type: "input",
+    message: "Enter the Contract Source File path?",
+  });
+  try {
+    const spinner = createSpinner("Compiling...").start();
+
+    const filePath = answers.Filepath;
+
+    /// Check the file path Should have .sol in the end
+    extension = filePath.slice(
+      (Math.max(0, filePath.lastIndexOf(".")) || Infinity) + 1
+    );
+
+    /// Get the file and then read it's content
+    const data = await fsPromises.readFile(
+      path.join(__dirname, filePath),
+      "utf8"
+    );
+
+    /// Call the Compile API with the SourceCode
+    const response = await fetch("./api/compile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sourceCode: data }),
+    });
+
+    /// Parse the Output and display that
+    // console.log(response);
+    const formattedResponse = (await response.json()).output;
+    spinner.stop();
+
+    if (response.status == 200) {
+      console.log("Successfully Compiled");
+    } else {
+      console.log("Successfully Compiled");
+      console.log(formattedResponse);
+    }
+  } catch (error) {
+    /// Show the Errors if present based on the API Res Code
+    console.log(error);
+  }
 }
 
 async function deploy() {
@@ -62,6 +114,8 @@ async function search() {
   /// Call the API to check if the Address does exit ??
   /// Show the info , or display the same info on a fixed link
 }
+
 // Processes
 console.clear();
 await welcome();
+await askTask();
